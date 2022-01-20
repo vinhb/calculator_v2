dbabf03ae3c85fbedd1c48838abe061552d60d3a
vinhb <vinh.bui10@gmail.com>
2022-01-19 23:05:21 -0800

    adding files to repo html structure

---
+++ script.js
@@ -0,0 +1,10 @@
+// calc needs to do the following
+// add
+// subtract
+// multiply
+// divide
+
+// create a function operate that takes 2 numbers
+// calls one of the above functions on the numbers
+
+// create function that populates the display with result from operate
